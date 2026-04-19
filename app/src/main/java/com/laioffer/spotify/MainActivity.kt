48e7aebde34966356b08fae90cf548d6ce6d3132
houxuc-rgb <houxuc@andrew.cmu.edu>
2026-04-19 19:01:39 -0400

    your commit message

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -2,17 +2,8 @@ package com.laioffer.spotify
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -24,73 +15,69 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import coil.compose.AsyncImage
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.laioffer.spotify.datamodel.Section
+import com.laioffer.spotify.database.DatabaseDao
+import com.laioffer.spotify.datamodel.Album
 import com.laioffer.spotify.network.NetworkApi
-import com.laioffer.spotify.network.NetworkModule
+import com.laioffer.spotify.repository.HomeRepository
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-// customized extend AppCompatActivity
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    //field injection
     @Inject
-    lateinit var networkApi: NetworkApi
+    lateinit var api: NetworkApi
+    @Inject
+    lateinit var databaseDao: DatabaseDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContent {
-//            SpotifyTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface {
-//                    Greeting("Android")
-//                }
-//
-//            }
-//        }
         setContentView(R.layout.activity_main)
 
+        // findElementById("")
         val navView = findViewById<BottomNavigationView>(R.id.nav_view)
-
-        // navHost, navController
-        // using navController to change the fragment in navHost
-
-        val navHostFragment =supportFragmentManager
+        val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
         val navController = navHostFragment.navController
         navController.setGraph(R.navigation.nav_graph)
-
         NavigationUI.setupWithNavController(navView, navController)
-
-        // https://stackoverflow.com/questions/70703505/navigationui-not-working-correctly-with-bottom-navigation-view-implementation
-        navView.setOnItemSelectedListener{
+        navView.setOnItemSelectedListener {
             NavigationUI.onNavDestinationSelected(it, navController)
             navController.popBackStack(it.itemId, inclusive = false)
             true
         }
-
-//        val retrofit = NetworkModule.provideRetrofit()
-//        val networkApi = retrofit.create(NetworkApi::class.java)
-        val call = networkApi.getHomeFeed()
-
-        GlobalScope.launch(Dispatchers.IO) {
-            val response: retrofit2.Response<List<Section>> = call.execute()
-            val sections: List<Section>? = response.body()
-            Log.d("Network", sections.toString())
+        // Test retrofit
+        lifecycleScope.launch(Dispatchers.IO) {
+            //val api = NetworkModule.provideRetrofit().create(NetworkApi::class.java)
+            val response = api.getHomeFeed().execute().body()
+            Log.d("Network", response.toString())
         }
+        // remember it runs everytime you start the app
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                val album = Album(
+                    id = 1,
+                    name =  "Hexagonal",
+                    year = "2008",
+                    cover = "https://upload.wikimedia.org/wikipedia/en/6/6d/Leessang-Hexagonal_%28cover%29.jpg",
+                    artists = "Lesssang",
+                    description = "Leessang (Korean: 리쌍) was a South Korean hip hop duo, composed of Kang Hee-gun (Gary or Garie) and Gil Seong-joon (Gil)"
+                )
+                databaseDao.favoriteAlbum(album)
+            }
+        }
+
     }
 }
-
 
 @Composable
 private fun LoadingSection(text: String) {
@@ -104,41 +91,32 @@ private fun LoadingSection(text: String) {
 }
 
 @Composable
-private fun AlbumCover() {
-    Column {
+fun AlbumCover() {
+    Column() {
         Box(modifier = Modifier.size(160.dp)) {
             AsyncImage(
                 model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.FillBounds,
+                contentScale = ContentScale.FillBounds
             )
 
-            Text(
+            Text( // ctrl + J
                 text = "Still Fantasy",
                 color = Color.White,
-                modifier = Modifier
-                    .padding(bottom = 4.dp, start = 2.dp)
-                    .align(Alignment.BottomStart),
+                modifier = Modifier.padding(bottom = 4.dp, start = 2.dp).align(Alignment.BottomStart)
             )
         }
-
         Text(
-            text = "Jay Chou",
+            text = "jay Chu",
             modifier = Modifier.padding(top = 4.dp),
-            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
-            color = Color.LightGray,
-        )
+            style=MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.White)
     }
 }
 
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", color = Color.White)
-}
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Preview LoadingSection")
 @Composable
 fun DefaultPreview() {
     SpotifyTheme {
--- app/src/main/java/com/laioffer/spotify/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/laioffer/spotify/ui/home/HomeViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.laioffer.spotify.datamodel.Section
 import com.laioffer.spotify.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -14,18 +15,28 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repository: HomeRepository): ViewModel() {
-    //state
-    //public immutable stateFlow, private mutable
     private val _uiState = MutableStateFlow(HomeUiState(feed = emptyList(), isLoading = true))
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
-    //event
+
     fun fetchHomeScreen() {
+        Log.d("Tim", Thread.currentThread().name + " 1")
         viewModelScope.launch {
+            Log.d("Tim", Thread.currentThread().name + " 2")
             val sections = repository.getHomeSections()
+
+            Log.d("Tim", Thread.currentThread().name + " 4")
             _uiState.value = HomeUiState(feed = sections, isLoading = false)
+
             Log.d("HomeViewModel", _uiState.value.toString())
         }
+        viewModelScope.launch {
+            delay(3000)
+            Log.d("Tim", Thread.currentThread().name + " 5")
+        }
+        Log.d("Tim", Thread.currentThread().name + " 6")
+
+
     }
 }
 
